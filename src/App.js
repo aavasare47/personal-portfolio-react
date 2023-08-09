4cf82d4b91913e1d7cbd19545b9cbf21ba15e4a4
aavasare47 <aniketavasare2004@gmail.com>
2023-08-09 23:05:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Navbar from "./Nav"
+
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <div className="navbar">
+            <ul className="navbar--option--list">
+                <li>00:Home</li>
+                <li>01:Projects</li>
+                <li>02:Resume</li>
+                <li>03:Blog</li>
+            </ul>
+        </div>
+    )
+}
