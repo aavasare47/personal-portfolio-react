7f5628eb6257b911367cfce1538d3a25507ddd72
aavasare47 <aniketavasare2004@gmail.com>
2023-08-10 13:43:18 +0530

    started hero

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Navbar from "./Nav"
-
+import Hero from "./Hero"
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   )
 }
---
+++ src/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Hero () {
+    return (
+        <div className="hero--containter">
+            <p className="hero--element1">
+                Hi! My name is
+            </p>
+            <img className="hero--element2" src='/assets/Ellipse 1.png' alt="my face"/>
+            <p className="hero--element3">ANIKET L. AVASARE</p>
+            <p className="hero--element4">I build things with code.</p>
+        </div>
+    )
+}
