e4ba4df2d1ffe0cb92111d029512e72c3a5419af
aavasare47 <aniketavasare2004@gmail.com>
2023-08-15 18:40:00 +0530

    about

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Navbar from "./Nav"
 import Hero from "./Hero"
+import About from "./About"
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <About />
     </div>
   )
 }
