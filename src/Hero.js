4168083cf71604aadfccfa8d3ecdb5f8ec24ea48
aavasare <aniketavasare2004@gmail.com>
2023-09-12 09:57:52 -0500

    nav tweaks

--- src/Hero.js
+++ src/Hero.js
@@ -8,7 +8,7 @@ export default function Hero () {
             </p>
             <img className="hero--element2" src='/assets/Ellipse 1.png' alt="my face"/>
             <p className="hero--element3">ANIKET L. AVASARE</p>
-            <p className="hero--element4">I build things with code.</p>
+            <div className="hero--element4">I build things with code.</div>
         </div>
     )
 }
